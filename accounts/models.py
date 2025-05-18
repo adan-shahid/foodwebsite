fd928d0280e8c3bb53aecb22416dbb69015596ab
Adan <adanshahid.student@gmail.com>
2025-05-18 10:01:12 +0500

    added the user model, extended from AbstractBasrUser

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 # Create your models here.
 
-class userManager(BaseUserManager):
+class userManager(BaseUserManager): #WE ARE EXTENDING THIS BASEUSERMANAGER, i.e, TAKING CONTROL OF IT.
     def create_user(self, first_name, last_name, username, email, password=None):
 
         if not email:
@@ -29,3 +29,52 @@ class userManager(BaseUserManager):
             last_name = last_name,
             password = password,
          )
+        user.is_admin = True
+        user.is_active = True
+        user.is_staff = True
+        user.is_superadmin = True
+
+        user.save(using=self._db)
+        return user
+    
+class User(AbstractBaseUser): # WE ARE TAKING FULL CONTROL OF CUSTOM USER MODEL, INCLUDING AUTHENTICATI0N.
+    RESTAURANT = 1
+    CUSTOMER = 2
+
+    ROLE_CHOICE = (
+        (RESTAURANT, 'Restaurant'),
+        (CUSTOMER, 'Customer'),
+    )
+    
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(max_length=100, unique= True)
+    phone = models.IntegerField(max_length=13, blank=True)
+
+    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICE, blank=True, null=True)
+
+
+    # REQUIRED FIELDS
+
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now_add=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    is_superadmin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email' #WE USE EMAIL AS A LOGIN FIELD
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    def __str__(self):
+        return self.email
+    
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+    
+    def has_module_perms(self, app_label):
+        return True

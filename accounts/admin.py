958399fdb8d833142c06dac4f3905546e301f559
Adan <adanshahid.student@gmail.com>
2025-05-20 15:39:08 +0500

    Updated admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 from .models import User
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
-admin.site.register(User)
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'username','role', 'is_active') #in which order, fields show in admin panel
+
+    ordering = ('-date_joined',)
+#TO MAKE THE PASSWORD NON-EDITABLE IN THE ADMIN PANEL.
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+
+
+admin.site.register(User, CustomUserAdmin)
 
 

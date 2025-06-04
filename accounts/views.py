fff19d389df9c8998e5582859aaac98521d17d07
Adan <adanshahid.student@gmail.com>
2025-06-04 12:43:57 +0500

    created the registerUser.html file, from onwards I will start implementing user registration form

--- accounts/views.py
+++ accounts/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def registerUser(request):
-    return HttpResponse("This is user reg form test.")
+    return render(request, 'accounts/registerUser.html')

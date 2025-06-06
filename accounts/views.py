34e2ce5c95239d1cf903e25f9cc5afd555b8c2d9
Adan <adanshahid.student@gmail.com>
2025-06-06 10:32:19 +0500

    there are 2 ways of hashing the password, one is done in the registerUser view, just before saving the form

--- accounts/views.py
+++ accounts/views.py
@@ -8,8 +8,10 @@ def registerUser(request):
         print(request.POST) #request.POST, WE ARE GETTING THE DATA HERE.
         form = userRegistrationForm(request.POST)
         if form.is_valid():
-            user = form.save(commit=False)
+            password = form.cleaned_data['password']
+            user = form.save(commit=False) #FORM IS READY TO BE SAVED, BUT NOT YET SAVED. BCZ OF COMMIT
             user.role = User.CUSTOMER
+            user.set_password(password)
             user.save()
             return redirect('registerUser')
     else:

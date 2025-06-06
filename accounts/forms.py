1d15736b24bc5cac086242730b028cfdcb6857ac
Adan <adanshahid.student@gmail.com>
2025-06-06 10:21:49 +0500

    Implemented the processing of form. Next, I will Hash the password form and also perform some validations

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from .models import User
 
 class userRegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget = forms.PasswordInput())
+    confirm_password = forms.CharField(widget = forms.PasswordInput())
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', 'email','password']
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .forms import userRegistrationForm
+from .models import User
 
 # Create your views here.
 def registerUser(request):
+    if request.method == 'POST':
+        print(request.POST) #request.POST, WE ARE GETTING THE DATA HERE.
+        form = userRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.role = User.CUSTOMER
+            user.save()
+            return redirect('registerUser')
+    else:
+        form = userRegistrationForm()
+    
     form = userRegistrationForm()
     context = {
         'form':form

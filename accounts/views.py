0879509ef8d74afffedf14cfae44d9c6778c8003
Adan <adanshahid.student@gmail.com>
2025-06-08 06:54:12 +0500

    Hashed the password using 2 ways, Now I will focus on form validaion part

--- accounts/views.py
+++ accounts/views.py
@@ -8,11 +8,26 @@ def registerUser(request):
         print(request.POST) #request.POST, WE ARE GETTING THE DATA HERE.
         form = userRegistrationForm(request.POST)
         if form.is_valid():
+            # CREATE THE USER, USING THE FORM
+
+            # password = form.cleaned_data['password']
+            # user = form.save(commit=False) #FORM IS READY TO BE SAVED, BUT NOT YET SAVED. BCZ OF COMMIT
+            # user.role = User.CUSTOMER
+            # user.set_password(password)
+            # user.save()
+
+            # CREATE THE USER, USING create_user METHOD.
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            username = form.cleaned_data['username']
             password = form.cleaned_data['password']
-            user = form.save(commit=False) #FORM IS READY TO BE SAVED, BUT NOT YET SAVED. BCZ OF COMMIT
+
+            user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
             user.role = User.CUSTOMER
-            user.set_password(password)
             user.save()
+            print("User is created")
+
             return redirect('registerUser')
     else:
         form = userRegistrationForm()

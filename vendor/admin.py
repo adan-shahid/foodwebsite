f4722865105f3c4c82ba839b6bf320d1d942e427
Adan <adanshahid.student@gmail.com>
2025-06-14 10:03:10 +0500

    Configured the Vendor Admin because some of the fields are not showing

--- vendor/admin.py
+++ vendor/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 
 from .models import Vendor
 
-# Register your models here.
-admin.site.register(Vendor)
+#IN THE VENDOR MODEL'S ADMIN PANEL, ONLY VENDOR NAME IS SHOWING. NO OTHER FIELD IS SHOWING.
+#TO SHOW OTHER FIELDS, WE ARE WRITING THIS LOGIC
+
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ('user', 'vendor_name','is_approved', 'created_at')
+    list_display_links = ('user', 'vendor_name')
+
+
+
+
+
+
+admin.site.register(Vendor, VendorAdmin)

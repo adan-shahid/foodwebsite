9875be6a3cb199dcef9cfdb755ebf9819c42e590
Adan <adanshahid.student@gmail.com>
2025-06-24 07:46:50 +0500

    Forgot password setup. Next we will send the forgot password validation link

--- accounts/views.py
+++ accounts/views.py
@@ -172,3 +172,12 @@ def custDashboard(request):
 @user_passes_test(check_role_vender)
 def vendorDashboard(request):
     return render(request, 'accounts/vendorDashboard.html')
+
+def forgot_password(request):
+    return render(request, 'accounts/forgot_password.html')
+
+def reset_password_validate(request, uidb64, token):
+    return 
+
+def reset_password(request):
+    return render(request, 'accounts/reset_password.html')

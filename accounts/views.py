08860482e87a84a5842e28629ce997679a651892
Adan <adanshahid.student@gmail.com>
2025-06-24 16:00:59 +0500

    Wrote the reset_password_validate function. Now I will write reset_password.html

--- accounts/views.py
+++ accounts/views.py
@@ -187,7 +187,7 @@ def forgot_password(request):
             #SEND PASSWORD RESET EMAIL.
             mail_subject = 'Reset your password.'
             email_template = 'accounts/emails/reset_password_email.html'
-            send_password_reset_email(request, user, mail_subject, email_template)
+            send_verification_email(request, user, mail_subject, email_template)
 
             messages.success(request, 'Password reset link has been sent to your email address.')
             return redirect('login')
@@ -199,7 +199,21 @@ def forgot_password(request):
     return render(request, 'accounts/forgot_password.html')
 
 def reset_password_validate(request, uidb64, token):
-    return 
+
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = User._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        request.session['uid'] = uid
+        messages.info(request, 'Please reset your password')
+        return redirect('reset_password')
+    else:
+        messages.error(request, 'This link has been expired!')
+        return redirect('myAccount')
+
 
 def reset_password(request):
     return render(request, 'accounts/reset_password.html')

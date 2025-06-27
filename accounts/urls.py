a3bd07eea28a8bfde7b179929c3ee074a1ea03eb
Adan <adanshahid.student@gmail.com>
2025-06-27 20:32:36 +0500

    Setting the URl of vendor Dashboard. Done with Dashboard and My Restaurant icons url setup

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 urlpatterns = [
+  
+    path('', views.myAccount),
     path('registerUser/', views.registerUser, name='registerUser'),
     path('registerVendor/', views.registerVendor, name='registerVendor'),
 
@@ -16,6 +18,9 @@ urlpatterns = [
     path('forgot_password/', views.forgot_password, name='forgot_password'),
     path('reset_password_validate/<uidb64>/<token>/', views.reset_password_validate, name='reset_password_validate'),
     path('reset_password/', views.reset_password, name='reset_password'),
+
+#WE WANT TO PASS THE URL REQUEST TO VENDOR APP.
+    path('vendor/', include('vendor.urls')),
 
 
 ]

e4d1063ec92da98ca253083ae18305006a32aa56
adan-shahid <adanshahid.student@gmail.com>
2025-07-01 20:58:02 +0500

    Got the previously stored data in the formns

--- vendor/views.py
+++ vendor/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .forms import vendorRegistrationForm
 from accounts.forms import userProfileForm
+from accounts.models import UserProfile
+from .models import Vendor
 
 # Create your views here.
 def vProfile(request):
-  profile_form = userProfileForm()
-  vendor_form = vendorRegistrationForm()
+  profile = get_object_or_404(UserProfile, user=request.user)
+  vendor = get_object_or_404(Vendor, user=request.user)
+
+#BY PASSING THE INSTANCE, WE ARE GETTING THE PREVIUOSLY STORED DATA IN THESE FORMS.
+  profile_form = userProfileForm(instance=profile)
+  vendor_form = vendorRegistrationForm(instance=vendor)
   context = {
     'profile_form':profile_form,
     'vendor_form':vendor_form,

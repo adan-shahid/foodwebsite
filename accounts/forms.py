ef2ac74d1bce32cfc00a50dd413367e4ca8f7530
adan-shahid <adanshahid.student@gmail.com>
2025-07-02 18:01:56 +0500

    Wrote the custom validator, but it is still not implemented because of errors which I don't know

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import User, UserProfile
+from .validators import allow_only_images_validator
+
 
 class userRegistrationForm(forms.ModelForm):
     password = forms.CharField(widget = forms.PasswordInput())
@@ -20,8 +22,9 @@ class userRegistrationForm(forms.ModelForm):
         
 class userProfileForm(forms.ModelForm):
     class Meta:
-        profile_picture = forms.ImageField(widget=forms.FileInput(attrs={'class':'btn btn-info'}))
-        cover_photo = forms.ImageField(widget=forms.FileInput(attrs={'class':'btn btn-info'}))
+
+        profile_picture = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
+        cover_photo = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
         
         model = UserProfile
         fields = ['profile_picture', 'cover_photo', 'address_line_1', 'address_line_2', 'country', 'state', 'city',

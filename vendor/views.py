c5c2efdc916023fab71d1210500f16a1ac5ebcb9
adan-shahid <adanshahid.student@gmail.com>
2025-07-03 21:13:18 +0500

    We added the decorators to vProfile view.

--- vendor/views.py
+++ vendor/views.py
@@ -6,7 +6,13 @@ from .models import Vendor
 
 from django.contrib import messages
 
+from django.contrib.auth.decorators import login_required, user_passes_test
+from accounts.views import check_role_vendor
+
 # Create your views here.
+
+@login_required(login_url='login')
+@user_passes_test(check_role_vendor)
 def vProfile(request):
   profile = get_object_or_404(UserProfile, user=request.user)
   vendor = get_object_or_404(Vendor, user=request.user)

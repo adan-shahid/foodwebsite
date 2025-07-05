b22589d852bcb755bb815239861488793e3769c2
adan-shahid <adanshahid.student@gmail.com>
2025-07-05 21:00:02 +0500

    Setup the admin panel for category model

--- menu/admin.py
+++ menu/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
-from .models import Category, foodItems
+from .models import Category, foodItem
+
 
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('category_name',)}
+    list_display = ('category_name', 'vendor', 'updated_at')
+    #SIMPL vendor  WILL NOT WORK, BCZ IT A FOREIGNKEY
+    search_fields = ('category_name', 'vendor__vendor_name')
+
+
+    
 
 # Register your models here.
 admin.site.register(Category,CategoryAdmin)
-admin.site.register(foodItems)
+admin.site.register(foodItem)
--- menu/models.py
+++ menu/models.py
@@ -19,7 +19,7 @@ class Category(models.Model):
 
 
 
-class foodItems(models.Model):
+class foodItem(models.Model):
     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     food_title = models.CharField(max_length=50)

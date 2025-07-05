1f99a137db4b6746a6954f12964c051ef171c7fc
adan-shahid <adanshahid.student@gmail.com>
2025-07-05 20:51:37 +0500

    Changed the admin setting. Now whenever i type category_name, slug will automatically be typed

--- menu/admin.py
+++ menu/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Category, foodItems
 
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('category_name',)}
+
 # Register your models here.
-admin.site.register(Category)
+admin.site.register(Category,CategoryAdmin)
 admin.site.register(foodItems)

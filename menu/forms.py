cab4eae143a095a62fe7e9017b8080b3c4fb7a04
adan-shahid <adanshahid.student@gmail.com>
2025-07-10 13:06:50 +0500

    fixed the image field css

--- menu/forms.py
+++ menu/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Category, foodItem
+from accounts.validators import allow_only_images_validator
 
 class categoryForm(forms.ModelForm):
     class Meta:
@@ -7,6 +8,7 @@ class categoryForm(forms.ModelForm):
         fields = ['category_name', 'description']
         
 class foodItemForm(forms.ModelForm):
+    image = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info w-100'}), validators=[allow_only_images_validator])
     class Meta:
         model = foodItem
         fields = ['category','food_title' ,'description' ,'price' ,'image' ,'is_availabe']

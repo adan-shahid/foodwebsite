88a845361e3a271754d29c91030fbc7dfc1732eb
adan-shahid <adanshahid.student@gmail.com>
2025-07-15 20:39:54 +0500

    Marketplace implementation started. Working on showing vendors on Home page

--- foodOnline_main/views.py
+++ foodOnline_main/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import HttpResponse, render
+from vendor.models import Vendor
 
 def home(request):
-    return render(request, 'home.html')
+    vendors = Vendor.objects.filter(is_approved = True, user__is_active =True) #WE ARE GETTING 'is_active' status from User model.
+    context = {
+        'vendors':vendors,
+    }
+    return render(request, 'home.html', context)

9c5cdc4ff3ef2fdf695c236e5463c4809d509108
adan-shahid <adanshahid.student@gmail.com>
2025-07-18 12:01:00 +0500

    limit the number of restaurants to shown on home page to 8

--- foodOnline_main/views.py
+++ foodOnline_main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import HttpResponse, render
 from vendor.models import Vendor
 
 def home(request):
-    vendors = Vendor.objects.filter(is_approved = True, user__is_active =True) #WE ARE GETTING 'is_active' status from User model.
+    vendors = Vendor.objects.filter(is_approved = True, user__is_active =True)[:8] #WE ARE GETTING 'is_active' status from User model.
     context = {
         'vendors':vendors,
     }

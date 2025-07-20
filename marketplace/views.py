f4d89136411a3757122eb18db3ea88f05a963429
adan-shahid <adanshahid.student@gmail.com>
2025-07-20 21:27:25 +0500

    implementing the listing section in Marketplace implementation

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from vendor.models import Vendor
 
 # Create your views here.
 def marketplace(request):
-    return render(request, 'marketplace/listings.html')
+    vendors = Vendor.objects.filter(is_approved = True, user__is_active =True)
+    vendor_count = vendors.count()
+    context = {
+        'vendors':vendors,
+        'vendor_count':vendor_count,
+
+    }
+#NOW ON THE listings.html, we have the access to vendors and vendor_count.
+    return render(request, 'marketplace/listings.html',context)
+

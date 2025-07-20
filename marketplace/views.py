efa766d3892b4c57cfa69be0154321cc4b164abb
adan-shahid <adanshahid.student@gmail.com>
2025-07-20 21:29:14 +0500

    lines added to count the number of vendors

--- marketplace/views.py
+++ marketplace/views.py
@@ -4,7 +4,7 @@ from vendor.models import Vendor
 # Create your views here.
 def marketplace(request):
     vendors = Vendor.objects.filter(is_approved = True, user__is_active =True)
-    vendor_count = vendors.count()
+    vendor_count = vendors.count() #TO COUNT THE NUMBER OF VENDORS.
     context = {
         'vendors':vendors,
         'vendor_count':vendor_count,

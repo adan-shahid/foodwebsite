8bd2ddb8bb94156b56e2170af15546f8718fa252
adan-shahid <adanshahid.student@gmail.com>
2025-07-20 21:49:26 +0500

    Added the view to generate slug field automatically

--- accounts/views.py
+++ accounts/views.py
@@ -10,6 +10,7 @@ from django.core.exceptions import PermissionDenied
 
 from django.utils.http import urlsafe_base64_decode
 from django.contrib.auth.tokens import default_token_generator
+from django.template.defaultfilters import slugify
 
 from vendor.models import Vendor
 
@@ -124,6 +125,8 @@ def registerVendor(request):
 
             vendor = v_form.save(commit=False)
             vendor.user = user
+            vendor_name = v_form.cleaned_data['vendor_name']
+            vendor.vendor_slug = slugify(vendor_name)
             user_profile = UserProfile.objects.get(user=user)
             vendor.user_profile = user_profile
             vendor.save()

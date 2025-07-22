f9903abbd64f31a367a61420fad5e5142390fe5a
adan-shahid <adanshahid.student@gmail.com>
2025-07-22 20:37:32 +0500

    Got the categories at vendor detail.html

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from vendor.models import Vendor
+from menu.models import Category
 
 # Create your views here.
 def marketplace(request):
@@ -15,8 +16,12 @@ def marketplace(request):
 
 def vendor_detail(request, vendor_slug):
     vendor = get_object_or_404(Vendor, vendor_slug=vendor_slug)
+
+    categories = Category.objects.filter(vendor=vendor) #BCZ WE ARE ONLY GETTING THE CATEGORIES OF THAT SPECIFIC VENDOR.
+
     context = {
         'vendor':vendor,
+        'categories':categories,
     }
     return render(request, 'marketplace/vendor_detail.html', context)
 

504fc94f4f5604c92a696fa9c9f8b3d2573a9f59
adan-shahid <adanshahid.student@gmail.com>
2025-08-15 11:01:14 +0500

    Created the context processor to get the count of the cart and registered it in setting.py file

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -70,6 +70,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'accounts.context_processor.get_vendor',  #APPNAME.FILE_NAME.FUNCTION_NAME
+                'marketplace.context_processor.get_cart_count',
             ],
         },
     },

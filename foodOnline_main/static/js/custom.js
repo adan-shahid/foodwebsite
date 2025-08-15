a92b00e8b3edd9778b28c586531732de710bf1ce
adan-shahid <adanshahid.student@gmail.com>
2025-08-15 09:00:16 +0500

    Our AJAX request is reaching to our add_to_cart function

--- foodOnline_main/static/js/custom.js
+++ foodOnline_main/static/js/custom.js
@@ -1,13 +1,16 @@
 $(document).ready(function(){
     $('.add_to_cart').on('click', function(e){
         e.preventDefault();
-        
+ 
+//WHEN I CLICK ON ADD_TO_CART BUTTON, IT WILL TAKE ALL THIS DATA.
         food_id = $(this).attr('data-id');
         url = $(this).attr('data-url');
         data = {
             food_id: food_id,
         }
         
+// WE ARE SENDING THAT 'food_id' TO OUR 'add_to_cart' view using the 
+// AJAX request
         $.ajax({
             type: 'GET',
             url: url,

947e69bed93656c686f9885a818972487ae5c99d
Adan Shahid <adanshahid.student@gmail.com>
2025-10-14 20:55:57 +0500

    Started again this project. Configured the project on my machine.

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -160,5 +160,6 @@ EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER') #EMAIL TO SEND MESSAGES TO USER.
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = True #EMAIL WILL BE SEND SECURELY.
+#we are using it in the utils.py file.
 DEFAULT_FROM_EMAIL = 'foodOnline Markerplace <adanshahid.engineer@gmail.com >'
